f5cbc9af3b1eecabe910c22a7d1d11fd27e320d0
Laura Canaia <lauracanaia99@gmail.com>
2023-12-27 12:00:18 +0100

    the python code now extracts the API's URLs

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # Automatic swagger specification #
 This project contains a .sh that joins different tools (like crawljax or mitmproxy) in order to help the automation of the 
 generation process of an API's specification. \
-This readme file tries to explain briefly the steps the .sh code does in order to generate the different swaggers.
+This readme file tries to explain briefly the steps of the .sh code does in order to generate the different swaggers.
 
 ### Python code ###
 The python code takes as input a flow from mitmdump tries to extract the different URLs of the different APIs
--- httpExtractor.py
+++ httpExtractor.py
@@ -1,44 +1,42 @@
 from mitmproxy.http import HTTPFlow
 from mitmproxy.io import FlowReader
 import os
-"""
-if f.request.scheme in ['http', 'https', 'get', 'post', 'put', 'delete', 'patch',
-                        'options', 'head', 'connect', 'trace', 'json']:
-    print(f"Request URL: {f.request.url}\n")
-    print(f"Request Method: {f.request.method}\n")
-    print(f"Request Headers: {f.request.headers}\n")
-    print(f"Request Body: {f.request.content}\n")
-    print("\n")
-    output_file.write(f"Request URL: {f.request.url}\n")
-    output_file.write(f"Request Method: {f.request.method}\n")
-    output_file.write(f"Request Headers: {f.request.headers}\n")
-    output_file.write(f"Request Body: {f.request.content}\n")
-    output_file.write("\n")
-    """
 
-def extract_json_responses(flow_file, outputfile):
+
+def extract_json_responses(flow_file):
+    API_list = []
     with open(flow_file, "rb") as f:
         flow_reader = FlowReader(f)
         # Check if the output file exists, it removes it
-        if os.path.exists(outputfile):
-            os.remove(outputfile)
-        # creates a new output file
-        with open(outputfile, 'a'):
-            pass  # Create an empty file
 
-        with open(outputfile, 'a') as output_file:
-            for f in flow_reader.stream():
-                # the function isistance returns true whether the first parameter is an istance of a class or a subclass
-                # specified in the second parameter (checks if f is an istance of HTTPFlow)
-                if isinstance(f, HTTPFlow):
-                    # check if in the content type field the substring 'json' is present
-                    if "json" in f.response.headers.get("Content-Type", ""):
-                        if f.request.method in ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD', 'CONNECT', 'TRACE'] :
-                            split_path = (f.request.path).split('/', 3)
-                            output_file.write(f"{f.type}://{f.request.authority}/{split_path[1]}\n")
-                            output_file.write(f"{f}\n")
-                            output_file.write("\n")
+        for f in flow_reader.stream():
+            # the function isistance returns true whether the first parameter is an istance of a class or a subclass
+            # specified in the second parameter (checks if f is an istance of HTTPFlow)
+            if isinstance(f, HTTPFlow):
+                # check if in the content type field the substring 'json' is present
+                if "json" in f.response.headers.get("Content-Type", ""):
+                    if f.request.method in ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD', 'CONNECT',
+                                            'TRACE']:
+                        split_path = (f.request.path).split('/', 3)
+                        resulting_string = f"{f.type}://{f.request.authority}/{split_path[1]}"
+                        if resulting_string not in API_list:
+                            API_list.append(resulting_string)
+
+    return API_list
+
+
+def write_in_output_file(outputfile, api_list):
+    if os.path.exists(outputfile):
+        os.remove(outputfile)
+        # creates a new output file
+    with open(outputfile, 'a'):
+        pass  # Create an empty file
+
+    with open(outputfile, 'a') as output:
+        for elem in api_list:
+            output.write(f"{elem}\n")
+
 
 # first filter to strain the http flow in order to get only the responses with json in it
-extract_json_responses('about.cilabs.com/flowDump', 'about.cilabs.com/output.txt')
-
+list_of_API = extract_json_responses('about.cilabs.com/flowDump')
+write_in_output_file('about.cilabs.com/output.txt', list_of_API)
